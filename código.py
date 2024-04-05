565da3292db4f41781ed48eaee8abc2d9f3d3a6a
Yasmin Naomi <yasminmin1704@gmail.com>
2024-04-05 19:33:37 -0300

    first commit

---
+++ código.py
@@ -0,0 +1,45 @@
+
+print("Primeira nota=", 2)
+print("segunda nota=", 3)
+print("terceira nota=", 5)
+print("Média final=", 2+3+5/3)
+print("Aluno aprovado", 2+3+5/3 > 8)
+print("aluno reprovado", 2+3+5/3 <8)
+
+
+print("Número inteiro para A,", 27)
+print("Número inteiro para B,", 65)
+print("A é maior que B", 27 > 65)
+print("B é maior que A", 65 > 27)
+
+
+10 + 20 * 30
+4**2 //30
+(9**4+2) *6 - 1
+print(" resultado:",10 + 20 * 30 )
+print("Resultado:", 4**2//30)
+print("resultado:", (9**4+2) *6 - 1)
+
+print("10%3*10**2+1-10*4/2")
+print("resultado:", 10%3*10**2+1-10*4/2)
+
+print("yasmin")
+
+a= 3
+b= 5
+print("resultado para a conta, 2a+3b= ", 2*a + 3*b)
+
+x= 8
+o=4
+p=3
+print("Soma das váriaveis: x, o e p =", x+o+p)
+
+
+print("Sálario do cliente:", 1456)
+print("Valor mínimo para pegar empréstimo no banco:", 1100)
+print("empréstimo aprovado", 1456 >= 1100)
+print("Empréstimo negado", 1456 <= 1100)
+
+
+a= input("digite o primeiro número")
+
